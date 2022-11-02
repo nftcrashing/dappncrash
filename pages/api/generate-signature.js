995dd7636a244e44d718020cf42cdae3750eea29
nftcrashing <nftcrashing1@gmail.com>
2022-11-02 20:18:55 +0400

    some possibly meaningful message

--- pages/api/generate-signature.js
+++ pages/api/generate-signature.js
@@ -2,16 +2,16 @@ import { ThirdwebSDK } from "@thirdweb-dev/sdk"
 
 const handler = async (req, res) => {
     const {address, amount} = req.body;
-    const sdk = ThirdwebSDK.fromPrivateKey(process.env.PRIVATE_KEY, "goerli",);
-    const contract = await sdk.getcontract("0xfF919Bb7Fff7DB653B065b211810d27944821591");
+    const sdk = ThirdwebSDK.fromPrivateKey(process.env.PRIVATE_KEY, "goerli");
+    const contract = await sdk.getContract("0xF0E1E7C95ef6C4DCBBB23c300E6A2E135DE624B7");
     const price = amount >= 2 ? 0.075 : 0.1;
     try {
-        const signedPayLoad = await contract.erc721.contract.generate({
+        const signedPayLoad = await contract.erc721.signature.generate({
             to: address,
             price,
             quantity: amount,
         });
-        return res.status(200).json({SignedPayload});
+        return res.status(200).json({signedPayload});
         
     } catch (error) {
         console.error(error);
